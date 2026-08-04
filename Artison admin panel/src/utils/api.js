e67c46a215be5e1bacdb00d75e78b49db8c41727
digvijay315 <digvijaykumar.315@gmail.com>
2026-08-04 16:18:34 +0530

    update'

--- Artison admin panel/src/utils/api.js
+++ Artison admin panel/src/utils/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://artison-fr5n.onrender.com/api',
+  // baseURL: 'https://artison-fr5n.onrender.com/api',
+  baseURL: 'https://api.kala-kosh.co.in/api',
  
   headers: {
     'Content-Type': 'application/json',
